064e50bcc962b603b0daa704cd31f981bf45f258
Juan <49213049+jcuyaguari@users.noreply.github.com>
2021-08-05 00:34:08 -0500

    add server - test

--- contracts/RegistroContract.sol
+++ contracts/RegistroContract.sol
@@ -5,9 +5,22 @@ contract RegistroContract {
 
     uint public registroCounter = 0;
 
-    constructor () public{
+    constructor (){
         createRegistro("Juan Carlos","199887699");
     }
+
+    event RegistroCreated(
+        uint id,
+        string titulo,
+        string descripcion,
+        bool done,
+        uint createdAt
+    );
+
+    event actualizaEstado(
+        uint id,
+        bool done
+    );
 
     struct Registro{
         uint id;
@@ -20,13 +33,16 @@ contract RegistroContract {
     mapping (uint256 => Registro) public registros;
 
     function createRegistro(string memory _titulo, string memory _descripcion)public{
-        registros[registroCounter] = Registro(registroCounter,_titulo,_descripcion,false,block.timestamp);//Blocktimestap guarda el tiempo en el q se esta creando la tarea
         registroCounter++;
+        registros[registroCounter] = Registro(registroCounter,_titulo,_descripcion,false,block.timestamp);//Blocktimestap guarda el tiempo en el q se esta creando la tarea
+        //Llamamos al evento
+        emit RegistroCreated(registroCounter,_titulo,_descripcion,false,block.timestamp);
     }
 
     function actualizarEstado(uint _id) public{
         Registro memory _registro = registros[_id];
         _registro.done = !_registro.done;
         registros[_id] = _registro;
+        emit actualizaEstado(_id,_registro.done);
     }
 }
--- test/RegistroContract.js
+++ test/RegistroContract.js
@@ -16,8 +16,37 @@ contract("RegistroContract",()=>{
 
     it('get Registros List', async()=>{
         const registroCounter = await this.registroContract.registroCounter()
-        const register = this.registroContract.registros(registroCounter)
+        const register = await this.registroContract.registros(registroCounter)
 
-        assert.equal(register.id.toNumber(),registroCounter-1)
+        assert.equal(register.id.toNumber(),registroCounter);
+        assert.equal(register.titulo, "Juan Carlos");
+        assert.equal(register.descripcion, "199887699");
+        assert.equal(register.done,false);
+        assert.equal(registroCounter,1);
+    })
+
+    it('registro created successfully',async()=>{
+        const result = await this.registroContract.createRegistro("Mercedes","123432123")
+        const registroEvent = result.logs[0].args;
+        const registrosCounter = await this.registroContract.registroCounter();
+
+        assert.equal(registrosCounter,2);
+        assert.equal(registroEvent.id.toNumber(),2);
+        assert.equal(registroEvent.titulo,"Mercedes");
+        assert.equal(registroEvent.descripcion,"123432123");
+        assert.equal(registroEvent.done,false);
+
+
+    })
+
+    it('registro created successfully',async()=>{
+        const result = await this.registroContract.actualizarEstado(1);
+        const registroEvento = result.logs[0].args;
+        const registro = await this.registroContract.registros(1)
+
+        assert.equal(registro.done,true);
+        assert.equal(registroEvento.done,true);
+        assert.equal(registroEvento.id,1);
+        
     })
 })
